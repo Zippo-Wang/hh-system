6de5067e7f43b7bdc435c8991b66566424f86114
Zippo-Wang <852420284@qq.com>
2024-01-08 20:17:22 +0800

    feat

--- utils/test.sh
+++ utils/test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-. $hh_project_path/utils/color.sh
-
 x1='tf'
 x2='init'
 x3="$x1$x2"
@@ -11,3 +9,4 @@ echo $x3
 # 字体颜色
 
 echo "${font_red}xxx${cend}"
+echo -e "\033[0;31m没有这个命令：\033[0mhh $@"
