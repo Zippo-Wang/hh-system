4dc88759ea2dcc6024a260d65fcf664076562f07
Zippo-Wang <852420284@qq.com>
2024-01-10 11:14:52 +0800

    feat

--- cmds/build_provider.sh
+++ cmds/build_provider.sh
@@ -9,14 +9,14 @@ f_build_huaweicloud(){
   make build
 
   echo -e "\033[0;36;40m[HuaweiCloud Provider] - Complete the build and start copying files to the local warehouse.\033[0m"
-  directory='~/.terraform.d/plugins/local-registry/huaweicloud/huaweicloud/0.0.1/linux_amd64'
+  directory='/home/huawei/.terraform.d/plugins/local-registry/huaweicloud/huaweicloud/0.0.1/linux_amd64'
 
   rm -rf "${directory}/terraform-provider-huaweicloud"
 
   if [ ! -d $directory ]
   then
     mkdir -p $directory
-    chmod -R 777 '~/.terraform.d/plugins/local-registry/huaweicloud/'
+    chmod -R 777 '/home/huawei/.terraform.d/plugins/local-registry/huaweicloud/'
   fi
 
   cp ~/go/bin/terraform-provider-huaweicloud $directory
@@ -34,14 +34,14 @@ f_build_flexibleengine(){
   make build
 
   echo -e "\033[0;36;40m[FlexibleEngineCloud Provider] - Complete the build and start copying files to the local warehouse.\033[0m"
-  directory='~/.terraform.d/plugins/local-registry/flexibleengine/flexibleengine/0.0.1/linux_amd64'
+  directory='/home/huawei/.terraform.d/plugins/local-registry/flexibleengine/flexibleengine/0.0.1/linux_amd64'
 
   rm -rf "${directory}/terraform-provider-flexibleengine"
 
   if [ ! -d $directory ]
   then
     mkdir -p $directory
-    chmod -R 777 '~/.terraform.d/plugins/local-registry/flexibleengine/'
+    chmod -R 777 '/home/huawei/.terraform.d/plugins/local-registry/flexibleengine/'
   fi
 
   cp ~/go/bin/terraform-provider-flexibleengine $directory
@@ -59,14 +59,14 @@ f_build_g42cloud(){
   make build
 
   echo -e "\033[0;36;40m[G42Cloud Provider] - Complete the build and start copying files to the local warehouse.\033[0m"
-  directory='~/.terraform.d/plugins/local-registry/g42cloud/g42cloud/0.0.1/linux_amd64'
+  directory='/home/huawei/.terraform.d/plugins/local-registry/g42cloud/g42cloud/0.0.1/linux_amd64'
 
   rm -rf "${directory}/terraform-provider-g42cloud"
 
   if [ ! -d $directory ]
   then
     mkdir -p $directory
-    chmod -R 777 '~/.terraform.d/plugins/local-registry/g42cloud/'
+    chmod -R 777 '/home/huawei/.terraform.d/plugins/local-registry/g42cloud/'
   fi
 
   cp ~/go/bin/terraform-provider-g42cloud $directory
