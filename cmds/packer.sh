99a620790b438570833a329e2c87665022fe9d37
Zippo-Wang <852420284@qq.com>
2024-05-14 16:53:39 +0800

    feat(upgrade): add upgrade

--- cmds/packer.sh
+++ cmds/packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 f_packer_build(){
-    printf "${level_info}${font_green1}执行 make install...${cend}\n"
+    printf "${level_info}${font_green1}[${main_hh} ${b_pkr}]执行中...${cend}\n"
     (cd ${hh_code_path}/packer-plugin-huaweicloud && make install)
-    printf "${level_info}${font_green1}[${main_hh} ${b_pkr}] 执行完成! ${cend}\n"
+    printf "${level_info}${font_green1}[${main_hh} ${b_pkr}]执行完成! ${cend}\n"
 }
